6b8d6512ce98810c95f824927262ee90812ef52d
Kiril-Pavlov <Kire_P@yahoo.com>
2022-11-01 20:47:51 +0100

    added a global styles file to the project

---
+++ src/components/styles/Global.js
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    *{
+        box-sizing:border-box;
+        padding: 0px;
+        margin: 0px;
+    }
+`
